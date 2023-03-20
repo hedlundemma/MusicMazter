b5b6267ca6647338a98d15d110b2efb6d4e9c039
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2023-03-20 15:47:04 +0100

    init

---
+++ MusicMazter/MusicMazter/Bank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MusicMazter;
+
+public class Bank
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+
+    public Bank(string name, double balance)
+    {
+
+        Name = name;
+        Balance = balance;
+    }
+}
+
